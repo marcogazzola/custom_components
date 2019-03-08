532065407fed299e810e8431742d58a1b80e5149
Marco Gazzola <dev@marcogazzola.com>
2019-03-08 01:35:19 +0100

    wifi status % split config code refactor refresh rate

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
 
+## [0.0.2b1](https://github.com/marcogazzola/custom_components/tree/0.0.2b1) (2019-03-08)
+
+- Splitting configuration
+- Code refactoring
+- Wifi sensor status is _wifi quality_ (0-100 %)
+- Refresh rate incresed
+
 ## [0.0.1](https://github.com/marcogazzola/custom_components/tree/0.0.1) (2019-03-06)
 
 - First release
--- shelly_cloud/sensor.py
+++ shelly_cloud/sensor.py
@@ -16,36 +16,15 @@ from homeassistant.const import (
     CONF_MONITORED_CONDITIONS
     )
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import (Entity, generate_entity_id)
 from homeassistant.util import Throttle
+from .const import *
+from . import VERSION
+from shellypython.const import (WORKING_MODE_RELAY, WORKING_MODE_ROLLER)
 
-REQUIREMENTS = ['shellypython>=0.0.3']
-
-__version__ = '0.0.2b0'
+REQUIREMENTS = ['shellypython>=0.0.4b0']
 
 _LOGGER = logging.getLogger(__name__)
-
-CONF_DOMAIN = 'domain'
-
-DEFAULT_NAME = 'Shelly'
-STATUS_URL = '/status'
-SENSOR_TYPES = ['MQTT', 'CLOUD', 'WIFI', 'FIRMWARE']
-
-CONST_CONNECTED = 'Connected'
-CONST_DISCONNECTED = 'Disconnected'
-CONST_ENABLED = 'Enabled'
-CONST_DISABLED = 'Disabled'
-CONST_UPTODATE = 'Latest'
-CONST_UPDATEAVAILABLE = 'New firmware available'
-CONST_BASE_SENSOR_TYPE = 'SYSTEM'
-
-SENSOR_ICONS = {
-    'MQTT': 'mdi:lan',
-    'CLOUD': 'mdi:cloud-sync',
-    'WIFI': 'mdi:wifi',
-    'FIRMWARE': 'mdi:cellphone-arrow-down',
-    CONST_BASE_SENSOR_TYPE: 'mdi:chip',
-}
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS):
@@ -59,7 +38,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the WHOIS sensor."""
+    """Set up the Shelly sensor."""
 
     ip_address = config.get(CONF_IP_ADDRESS)
     name = config.get(CONF_NAME)
@@ -67,16 +46,24 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     password = config.get(CONF_PASSWORD)
     monitored_condition = config[CONF_MONITORED_CONDITIONS]
 
-    shelly_data = ShellyData(ip_address, username, password, 30)
+    shelly_data = ShellyData(
+        ip_address, username, password, SCAN_INTERVAL_SECONDS, add_entities)
     shelly_data.update()
 
-    SENSOR_TYPES.append(CONST_BASE_SENSOR_TYPE)
-    monitored_condition.append(CONST_BASE_SENSOR_TYPE)
+    monitored_condition.append(CONST_SENSOR_SYSTEM)
+
+    if shelly_data is not None and shelly_data.data is not None:
+        if shelly_data.data.working_mode_raw == WORKING_MODE_RELAY:
+            if CONST_SENSOR_ROLLER in monitored_condition:
+                monitored_condition.remove(CONST_SENSOR_ROLLER)
+        elif shelly_data.data.working_mode_raw == WORKING_MODE_ROLLER:
+            if CONST_SENSOR_RELAY in monitored_condition:
+                monitored_condition.remove(CONST_SENSOR_RELAY)
 
     _LOGGER.info('if you have ANY issues with this, please report them here:'
                  ' https://github.com/marcogazzola/custom_components')
 
-    _LOGGER.debug('Version %s', __version__)
+    _LOGGER.debug('Version %s', VERSION)
 
     sensors = []
     for variable in monitored_condition:
@@ -114,7 +101,7 @@ class ShellySensor(Entity):
         if self.type in SENSOR_ICONS:
             return SENSOR_ICONS[self.type]
 
-        return SENSOR_ICONS[CONST_BASE_SENSOR_TYPE]
+        return SENSOR_ICONS[CONST_SENSOR_SYSTEM]
 
     @property
     def state(self):
@@ -141,7 +128,7 @@ class ShellySensor(Entity):
             self._empty_state_and_attributes()
             return
 
-        if self.type == CONST_BASE_SENSOR_TYPE:
+        if self.type == CONST_SENSOR_SYSTEM:
             self._state = self.shelly_data.data.main_status
             attributes_data = (
                 self.shelly_data.data.system.as_dict()
@@ -157,7 +144,7 @@ class ShellySensor(Entity):
                 )
 
             self._attributes = attributes_data
-        elif self.type == "MQTT":
+        elif self.type == CONST_SENSOR_MQTT:
             attributes_data = (
                 self.shelly_data.data.mqtt
                 if self.shelly_data.data.mqtt is not None
@@ -167,7 +154,7 @@ class ShellySensor(Entity):
             else:
                 self._state = CONST_CONNECTED
             self._attributes = None
-        elif self.type == "CLOUD":
+        elif self.type == CONST_SENSOR_CLOUD:
             attributes_data = (
                 self.shelly_data.data.cloud
                 if self.shelly_data.data.cloud is not None
@@ -177,7 +164,7 @@ class ShellySensor(Entity):
             else:
                 self._state = CONST_CONNECTED
             self._attributes = None
-        elif self.type == "WIFI":
+        elif self.type == CONST_SENSOR_WIFI:
             attributes_data = (
                 self.shelly_data.data.wifi_sta
                 if self.shelly_data.data.wifi_sta is not None
@@ -186,13 +173,29 @@ class ShellySensor(Entity):
                 self._state = CONST_DISCONNECTED
                 self._attributes = None
             else:
-                self._state = CONST_CONNECTED
+                self._state = "{} %".format(attributes_data.quality)
                 self._attributes = {
                     "Ssid": attributes_data.ssid,
                     "Ip": attributes_data.ip,
                     "Rssi": attributes_data.rssi,
                     }
-        elif self.type == "FIRMWARE":
+        elif self.type == CONST_SENSOR_FIRMWARE:
+            attributes_data = (
+                self.shelly_data.data.firmware
+                if self.shelly_data.data.firmware is not None
+                else None)
+            if attributes_data is None or not attributes_data.has_update:
+                self._state = CONST_UPTODATE
+                self._attributes = (
+                    {"Current version": attributes_data.old_version}
+                    )
+            else:
+                self._state = CONST_UPDATEAVAILABLE
+                self._attributes = {
+                    "Current version": attributes_data.old_version,
+                    "Latest version": attributes_data.new_version,
+                    }
+        elif self.type == CONST_SENSOR_FIRMWARE:
             attributes_data = (
                 self.shelly_data.data.firmware
                 if self.shelly_data.data.firmware is not None
@@ -209,12 +212,11 @@ class ShellySensor(Entity):
                     "Latest version": attributes_data.new_version,
                     }
 
-
 class ShellyData:
     """Get the latest data from ShellyData."""
 
     def __init__(
-            self, ip_address, username, password, scan_interval):
+            self, ip_address, username, password, scan_interval, add_entities):
         """Initialize the data object."""
         self._ip_address = ip_address
         self._username = username
